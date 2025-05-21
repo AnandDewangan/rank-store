1bbcefb6babf6b8171e8a7d2bea535fcda30b665
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2025-05-21 16:10:40 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import Home from "./pages/Home";
 import AllBooks from './pages/AllBooks';
 import BookDetail from "./pages/BookDetail";
 import CartPage from "./pages/CartPage";
+import TermConditions from "./pages/TermConditions";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import ContactUs from "./pages/ContactUs";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import FooterBottom from "./components/FooterBottom";
+import { ToastContainer } from 'react-toastify';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import AOS from "aos";
@@ -25,11 +29,15 @@ function App() {
   return (
     <Router>
       <Header />
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/books" element={<AllBooks />} />
         <Route path="/books/:id" element={<BookDetail />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/term-conditions" element={<TermConditions />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/contact-us" element={<ContactUs />} />
       </Routes>
       <Footer />
       <FooterBottom />
